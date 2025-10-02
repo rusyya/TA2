8e6e25c06e61dae3dfc4a496994468554ffc0687
rusyya <ya1107ruslan@gmail.com>
2025-10-02 17:46:21 +0300

    Passenger's init added

--- airport/passenger.py
+++ airport/passenger.py
@@ -4,4 +4,9 @@ __all__ = ['Passenger']
 
 
 class Passenger:
-    pass
+    def __init__(self, passport: str, name: str, surname: str, date_of_birth: str):
+        self._passport = passport
+        self._name = name
+        self._surname = surname
+        self._date_of_birth = datetime.strptime(date_of_birth, "%Y-%m-%d").date()
+        self._booked_flights = []
